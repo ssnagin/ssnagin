84f3d5672c3bcd60e5ce709d2c2206f4932b24e2
Stanislav Snagin <s_lstoo@bk.ru>
2026-02-06 01:24:58 +0300

    some night work in progress

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+
+  modules: [
+    '@nuxt/tailwindcss',
+    '@nuxt/ui'
+  ],
+
+  app: {
+    head: {
+      title: 'ssngn.ru | Дальше уже некуда',
+      meta: [
+        { name: 'ssngn.ru', content: 'Дальше уже некуда' }
+      ]
+    }
+  },
+
   devtools: { enabled: true }
 })
